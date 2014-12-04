f1786c9f05b89f1cc7d8b502981e776560902bc8
Thomas Brus <thomas.brus@me.com>
2014-12-04 18:46:14 +0100

    Broadcast before_{create,update,destroy} events as well

--- lib/wisper/active_record/publisher.rb
+++ lib/wisper/active_record/publisher.rb
@@ -2,14 +2,20 @@ module Wisper
   module ActiveRecord
     module Publisher
       extend ActiveSupport::Concern
-      included do
 
+      included do
         include Wisper::Publisher
 
         after_validation :after_validation_broadcast
+
+        before_create    :before_create_broadcast
+        before_update    :before_update_broadcast
+        before_destroy   :before_destroy_broadcast
+
         after_commit     :after_create_broadcast,  on: :create
         after_commit     :after_update_broadcast,  on: :update
         after_commit     :after_destroy_broadcast, on: :destroy
+
         after_rollback   :after_rollback_broadcast
       end
 
@@ -38,14 +44,26 @@ module Wisper
         broadcast("create_#{self.class.model_name.param_key}_successful", self)
       end
 
+      def before_create_broadcast
+        broadcast(:before_create, self)
+      end
+
       def after_update_broadcast
         broadcast(:after_update, self)
         broadcast("update_#{self.class.model_name.param_key}_successful", self)
       end
 
+      def before_update_broadcast
+        broadcast(:before_update, self)
+      end
+
       def after_destroy_broadcast
         broadcast(:after_destroy, self)
         broadcast("destroy_#{self.class.model_name.param_key}_successful", self)
+      end
+
+      def before_destroy_broadcast
+        broadcast(:before_destroy, self)
       end
 
       def after_rollback_broadcast
